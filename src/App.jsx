44679f45a3c2d9f13eeed91315f58b36541efcba
jyuuuuu0 <s25023@gsm.hs.kr>
2025-09-21 22:56:50 +0900

    전체 코드 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { useState, useEffect } from "react";
+import {useState,useEffect} from "react";
 import axios from "axios";
 
 const API_KEY = "c42d96c896b35e6dfe530cd973564856";
 
-export default function App() {
-  const [city, setCity] = useState("");         
-  const [weather, setWeather] = useState(null); 
-  const [recent, setRecent] = useState([]);     
+export default function Weather(){
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [recent, setRecent] = useState([]);
 
-  useEffect(() => {
-    const saved = JSON.parse(localStorage.getItem("recentCities"));
-    if (saved) {
+  useEffect(()=>{
+    const saved = JSON.parse(localStorage.getItem("weather"));
+    if(saved){
       setRecent(saved);
-      fetchWeather(saved[0]); 
+      fetchWeather(saved[0]);
     }
   }, []);
 
   const fetchWeather = async (cityName) => {
     if (!cityName) return;
 
-    try {
+    try{
       const res = await axios.get(
         "https://api.openweathermap.org/data/2.5/weather",
         {
@@ -32,46 +32,37 @@ export default function App() {
         }
       );
 
-      if (res.status === 200) {
-        if (weather) {
+      if(res.status === 200){
+        if(weather){
           setRecent([weather.name]);
-          localStorage.setItem("recentCities", JSON.stringify([weather.name]));
+          localStorage.setItem("weather", JSON.stringify([weather.name]));
         }
         setWeather(res.data);
       }
-    } catch (err) {
-      if (err.response && err.response.status === 404) {
+    } catch(err) {
+      if(err.status && err.status === 404)
         alert("지역을 찾을 수 없습니다.");
-      } else {
-        console.error("API 요청 에러:", err);
-        alert("날씨 정보를 가져오지 못했습니다.");
-      }
     }
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
     fetchWeather(city);
-    setCity(""); 
+    setCity("");
   };
 
-  return (
+  return(
     <div>
-      <h1>날씨 검색</h1>
+      <h1>🌤️날씨 검색</h1>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          placeholder="도시 이름 입력"
-        />
+        <input type="text" value={city} onChange={(e)=>setCity(e.target.value)} placeholder="🔍지역을 검색해주세요."/>
         <button type="submit">검색</button>
       </form>
 
-      {weather && (
+      {weather&&(
         <div>
-          <h2>{weather.name} 날씨</h2>
-          <p>기온: {weather.main.temp}°C</p>
+          <h2>{weather.name}</h2>
+          <p>기온: {weather.main.temp}</p>
           <p>날씨: {weather.weather[0].description}</p>
           <p>습도: {weather.main.humidity}%</p>
           <p>풍속: {weather.wind.speed}m/s</p>
